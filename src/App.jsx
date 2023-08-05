46e22535b71605ac28f915d924f5e6bb01fb3fe4
Bibhuti <biswalbibhuti8@gmail.com>
2023-08-05 07:57:51 +0530

    fetching the data and showing on screen

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { getApiConfiguration } from './store/homeSlice';
 function App() {
 
   const dispatch = useDispatch();
+  const {url}=useSelector((state)=>state.home);
+  console.log(url);
 
   useEffect(() => {
     apiTest();
@@ -22,7 +24,12 @@ function App() {
       });
   }
 
-  return <div className='app'>App</div>
+  return <div className='app'>
+    App
+    {
+      url?.total_pages
+    }
+    </div>
 
 }
 
